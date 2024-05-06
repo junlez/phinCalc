bfa98c170c98edb72b9d538e9af441a0e8fb9c76
Junle Zhang <zhangjunle@gmail.com>
2024-05-05 17:24:17 -0700

    1. updated excel with yearly breaddown and variable tax rate 2. updated script to work with new excel layout 3. variable tax rate still needs to be hooked up in the script

--- phinCalc.py
+++ phinCalc.py
@@ -2,133 +2,90 @@
 from openpyxl import load_workbook
 
 
+
 excelFile = 'phinCalc.xlsx'
+
+
+
+wb = load_workbook(excelFile, data_only = True)
+sheet = wb.worksheets[0]
+
+
+information = sheet['E7':'E10']
+income = sheet['D39':'E139']
+expense = sheet['F39':'G139']
+
+
+interestRateOnBalance = information[2][0].value/100.0 + 1
+balanceAmount = information[3][0].value
+
+
+taxRate = 1.0 - 30.0/100.0 #todo: hook up variable tax rate calculation
+
+
+balance = []
+
+if len(income) != len(expense):
+	print('ERROR: income length does not match expense')
+	quit()
+
+for index in range(len(income)):
+
+	balanceAmount = balanceAmount * interestRateOnBalance
+
+
+	yearlyIncome = 0.0
+
+	incomeAmount = income[index][0].value
+	if incomeAmount != None:
+		yearlyIncome = yearlyIncome + incomeAmount
+
+	incomeAmount = income[index][1].value
+	if incomeAmount != None:
+		incomeAmount = 12 * incomeAmount
+		yearlyIncome = yearlyIncome + incomeAmount
+
+	yearlyIncome = taxRate * yearlyIncome
+
+
+	yearlyExpense = 0.0
+
+	expenseAmount = expense[index][0].value
+	if expenseAmount != None:
+		yearlyExpense = yearlyExpense + expenseAmount
+
+	expenseAmount = expense[index][1].value
+	if expenseAmount != None:
+		expenseAmount = 12 * expenseAmount
+		yearlyExpense = yearlyExpense + expenseAmount
+
+
+	yearlyNet = yearlyIncome - yearlyExpense
+	
+	if yearlyNet < 0.0:
+		yearlyNet = yearlyNet / taxRate
+	
+	balanceAmount = balanceAmount + yearlyNet
+
+
+	balance.append([balanceAmount, yearlyNet])
+
+
+wb.close()
+
 
 
 wb = load_workbook(excelFile)
 sheet = wb.worksheets[0]
 
 
-initialInvestment = sheet['B5':'D9']
-income = sheet['B16':'H25']
-expense = sheet['B32':'H41']
-otherInformation = sheet['C46':'D50']
-
-
-balanceAmount = 0.0
-
-for row in initialInvestment:
-	cellValue = row[2].value
-	if cellValue is not None:
-		balanceAmount = balanceAmount + cellValue
-
-print(balanceAmount)
-
-
-startYear = otherInformation[0][1].value
-startAge = otherInformation[1][1].value
-endAge = otherInformation[2][1].value
-interestRateOnBalance = otherInformation[3][1].value/100.0 + 1
-taxRate = 1 - otherInformation[4][1].value/100.0
-
-balance = []
-# balance.append([startYear, balance])
-# print(balance)
-
-for index, age in enumerate(range(startAge, endAge + 1)):
-
-	year = startYear + index
-
-
-	balanceAmount = balanceAmount * interestRateOnBalance
-
-
-	yearlyIncome = 0.0
-	
-	for row in income:
-		incomeAmount = row[2].value
-		if incomeAmount == None:
-			continue
-
-		incomeYear = row[3].value
-		if incomeYear == None:
-			continue
-
-		if year == incomeYear:
-			yearlyIncome = yearlyIncome + incomeAmount
-
-	for row in income:
-		incomeAmount = row[4].value
-		if incomeAmount == None:
-			continue
-		incomeAmount = 12 * incomeAmount
-
-		incomeStartYear = row[5].value
-		if incomeStartYear == None:
-			continue
-
-		incomeEndYear = row[6].value
-		if incomeEndYear == None:
-			continue
-
-		if year >= incomeStartYear and year <= incomeEndYear:
-			yearlyIncome = yearlyIncome + incomeAmount
-
-	yearlyIncome = taxRate * yearlyIncome
-
-
-	yearlyExpense = 0.0
-	
-	for row in expense:
-		expenseAmount = row[2].value
-		if expenseAmount == None:
-			continue
-
-		expenseYear = row[3].value
-		if expenseYear == None:
-			continue
-
-		if year == expenseYear:
-			yearlyExpense = yearlyExpense + expenseAmount
-
-	for row in expense:
-		expenseAmount = row[4].value
-		if expenseAmount == None:
-			continue
-		expenseAmount = 12 * expenseAmount
-
-		expenseStartYear = row[5].value
-		if expenseStartYear == None:
-			continue
-
-		expenseEndYear = row[6].value
-		if expenseEndYear == None:
-			continue
-
-		if year >= expenseStartYear and year <= expenseEndYear:
-			yearlyExpense = yearlyExpense + expenseAmount
-
-
-	yearlyNet = yearlyIncome - yearlyExpense
-	
-	if yearlyNet < 0:
-		yearlyNet = yearlyNet / taxRate
-	
-	balanceAmount = balanceAmount + yearlyNet
-
-
-	balance.append([age, year, balanceAmount, yearlyNet])
-
-
 for rowIndex, row in enumerate(balance):
 	for columnIndex, column in enumerate(row):
-		sheet.cell(row = 56 + rowIndex, column = 4 + columnIndex).value = column
+		sheet.cell(row = 39 + rowIndex, column = 8 + columnIndex).value = column
 	print(row)
 
 
 wb.save(excelFile)
-
-
 
 
 
