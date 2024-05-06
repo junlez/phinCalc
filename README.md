5d2ecfcc69a8344f408d3f78eba7372c3d106a03
Junle Zhang <zhangjunle@gmail.com>
2024-05-05 22:41:25 -0700

    1. various improvements on tax calculation to improve accuracy 2. other minor excel file updates 3. added README

---
+++ README.md
@@ -0,0 +1,16 @@
+
+description:
+retirement and financial planning tool
+
+
+dependencies:
+-Microsoft Excel
+-Python 3
+-Python Module: openpyxl
+
+
+future todos:
+-add a main function to the python script and pass in the excel file as a parameter
+-in the excel file, post the equations used in the python script for reference
+
+
--- phinCalc.py
+++ phinCalc.py
@@ -7,22 +7,77 @@ excelFile = 'phinCalc.xlsx'
 
 
 
+def calculateTax(income, taxBrackets):
+
+	taxAmount = 0.0
+
+
+	previousTaxThreshold = 0.0
+	for taxBracket in taxBrackets[:-1]:
+		
+		taxRate = taxBracket[0]/100.0
+		taxThreshold = taxBracket[2]
+		
+		if income <= taxThreshold:
+			taxableAmount = income - previousTaxThreshold
+			return taxAmount + taxableAmount * taxRate
+		
+		taxableAmount = taxThreshold - previousTaxThreshold
+		taxAmount = taxAmount + taxableAmount * taxRate
+
+		previousTaxThreshold = taxThreshold
+
+
+	taxBracket = taxBrackets[-1]
+
+	taxRate = taxBracket[0]/100.0
+	taxableAmount = income - previousTaxThreshold
+	return taxAmount + taxableAmount * taxRate
+
+
+
+def calculateFederalAndStateTax(income, federalTaxBrackets, stateTaxBrackets):
+
+	return calculateTax(income, federalTaxBrackets) + calculateTax(income, stateTaxBrackets)
+
+
+
 wb = load_workbook(excelFile, data_only = True)
 sheet = wb.worksheets[0]
 
 
-information = sheet['E7':'E10']
-income = sheet['D39':'E139']
-expense = sheet['F39':'G139']
+information = sheet['E7':'E12']
+federalTaxRates = sheet['C17':'E23']
+stateTaxRates = sheet['C28':'E36']
+income = sheet['D41':'E123']
+expense = sheet['F41':'G123']
 
 
-interestRateOnBalance = information[2][0].value/100.0 + 1
-balanceAmount = information[3][0].value
+additionalIncomeTax = 1.0 - information[2][0].value/100.0
+capitalGainsInterest = information[3][0].value/100.0 + 1
+capitalGainsTax = 1.0 - information[4][0].value/100.0
+initialInvestiment = information[5][0].value
+# print(additionalIncomeTax)
+# print(capitalGainsInterest)
+# print(capitalGainsTax)
+# print(initialInvestiment)
+
+federalTaxBrackets = []
+for row in federalTaxRates:
+	taxBracket = []
+	for column in row:
+		taxBracket.append(column.value)
+	federalTaxBrackets.append(taxBracket)
+
+stateTaxBrackets = []
+for row in stateTaxRates:
+	taxBracket = []
+	for column in row:
+		taxBracket.append(column.value)
+	stateTaxBrackets.append(taxBracket)
 
 
-taxRate = 1.0 - 30.0/100.0 #todo: hook up variable tax rate calculation
-
-
+balanceAmount = initialInvestiment
 balance = []
 
 if len(income) != len(expense):
@@ -31,7 +86,7 @@ if len(income) != len(expense):
 
 for index in range(len(income)):
 
-	balanceAmount = balanceAmount * interestRateOnBalance
+	balanceAmount = balanceAmount * capitalGainsInterest
 
 
 	yearlyIncome = 0.0
@@ -45,7 +100,7 @@ for index in range(len(income)):
 		incomeAmount = 12 * incomeAmount
 		yearlyIncome = yearlyIncome + incomeAmount
 
-	yearlyIncome = taxRate * yearlyIncome
+	yearlyIncome = yearlyIncome * additionalIncomeTax - calculateFederalAndStateTax(yearlyIncome, federalTaxBrackets, stateTaxBrackets)
 
 
 	yearlyExpense = 0.0
@@ -63,7 +118,7 @@ for index in range(len(income)):
 	yearlyNet = yearlyIncome - yearlyExpense
 	
 	if yearlyNet < 0.0:
-		yearlyNet = yearlyNet / taxRate
+		yearlyNet = yearlyNet / capitalGainsTax
 	
 	balanceAmount = balanceAmount + yearlyNet
 
@@ -81,7 +136,7 @@ sheet = wb.worksheets[0]
 
 for rowIndex, row in enumerate(balance):
 	for columnIndex, column in enumerate(row):
-		sheet.cell(row = 39 + rowIndex, column = 8 + columnIndex).value = column
+		sheet.cell(row = 41 + rowIndex, column = 8 + columnIndex).value = column
 	print(row)
 
 
